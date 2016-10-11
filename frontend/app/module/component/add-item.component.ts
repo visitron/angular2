900e6a6eda02f9951ed6f7a6a47ed2705f037a93
Buibi <v.soshyn@gmail.com>
2016-10-11 23:51:00 +0300

    Created skeleton

--- frontend/app/module/component/add-item.component.ts
+++ frontend/app/module/component/add-item.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from "@angular/core";
+import {Item} from "./Item";
+declare var $:any;
+declare var _:any;
 
 @Component({
     selector: 'addItemDialog',
@@ -7,8 +10,33 @@ import {Component, OnInit} from "@angular/core";
 
 export class AddItemComponent implements OnInit {
 
+    public item: Item = new Item;
+
     ngOnInit(): void {
-        $('[data-toggle="toggle"]').each(function(index: number, elem: any) {$(elem).bootstrapToggle()});
+        let item0: Item = this.item;
+        $('[data-toggle="toggle"]').each(function(index: number, elem: any) {
+            $(elem).bootstrapToggle();
+            $(elem).change(function(val: any) {
+                if (_.isEmpty(val.target.id)) {
+                    console.error('All ids for toggle elements have to be defined [' + val.target.outerHTML);
+                    return;
+                }
+                console.log("Changed! => id=[" + val.target.id + "] " + val.target.checked);
+                item0.advanced[val.target.id] = val.target.checked;
+            });
+        });
+
+        this.item.name = 'Fridge';
+        this.item.description = 'Fridge for storing products';
+        this.item.lifecycle = 360;
+        this.item._maintenanceDate = new Date();
+
+        this.item.advanced.requiresSpecialist = false;
+        this.item.advanced.requiresAdditionalDetails = false;
+    }
+
+    public update(): void {
+
     }
 
 }
