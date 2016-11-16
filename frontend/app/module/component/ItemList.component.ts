f9832227bc5976f63909629e3f5ead0e274baec4
Buibi <v.soshyn@gmail.com>
2016-11-16 23:23:43 +0200

    reconciling ts models to java

--- frontend/app/module/component/ItemList.component.ts
+++ frontend/app/module/component/ItemList.component.ts
@@ -26,7 +26,7 @@ export class ItemListComponent implements OnInit {
     }
 
     calcDaysRemains(item: Item): number {
-        let diff: number = moment().diff(moment(item.maintenanceDate), "days");
+        let diff: number = moment().diff(moment(item.maintenanceDate, 'yyyy-MM-dd'), "days");
         return (item.lifecycle - diff) * 100 / item.lifecycle;
     }
 
