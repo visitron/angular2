381f3546f42deb297773bfc2ad5694841ae08616
Vladimir Soshyn <vladimir.soshyn@db.com>
2016-12-12 15:03:30 +0200

    introduced config for the app and service that accepts it

--- frontend/app/module/app.module.ts
+++ frontend/app/module/app.module.ts
@@ -7,12 +7,26 @@ import {ItemListComponent} from "./component/ItemList.component";
 import {AddItemComponent} from "./component/AddItem.component";
 import {ItemService} from "./component/Item.service";
 import {NotificationService} from "./component/notification.service";
+import {Paths, PathsFactory} from "./component/path.component";
+
+/**
+ * App constants:
+ * 'BACKEND_MODE' = 'mock' | 'real'
+ */
+
+const APP_PARAMS = {
+    "BACKEND_MODE": "mock"
+};
 
 @NgModule({
     imports: [BrowserModule, FormsModule, UiSwitchModule, HttpModule],
     declarations: [ItemListComponent, AddItemComponent],
-    providers: [ItemService, NotificationService],
+    providers: [
+        ItemService,
+        NotificationService,
+        {provide: "APP_PARAMS", useValue: APP_PARAMS},
+        {provide: Paths, useFactory: PathsFactory, deps: ["APP_PARAMS"]}
+    ],
     bootstrap: [ItemListComponent]
 })
-
 export class MaintenanceModule {}
--- frontend/app/module/component/Item.service.ts
+++ frontend/app/module/component/Item.service.ts
@@ -4,20 +4,21 @@ import {Item} from "./Item";
 import {Observable} from "rxjs";
 import "rxjs/add/operator/map";
 import "rxjs/add/operator/share";
+import {Paths} from "./path.component";
 
 @Injectable()
 export class ItemService {
 
     private emitter: EventEmitter<Item[]> = new EventEmitter<Item[]>();
 
-    constructor(private http: Http) {};
+    constructor(private http: Http, private paths: Paths) {};
 
     public loadItems(): void {
         let headers: Headers = new Headers;
         headers.append("Access-Control-Allow-Origin", "*");
         let options: RequestOptions = new RequestOptions({headers: headers});
         this.http
-            .get("/angular2/frontend/mock/items.json", options)
+            .get(this.paths.ITEM.GET_ALL, options)
             .map((response: Response) => response.json() as Item[])
             .subscribe(items => {
                 this.emitter.emit(items);
@@ -32,7 +33,7 @@ export class ItemService {
         let headers: Headers = new Headers({'Content-Type': 'application/json'});
         let options: RequestOptions = new RequestOptions({headers: headers});
         return this.http
-            .post("http://localhost:7001/home-maintenance/item/save", item, options)
+            .post(this.paths.ITEM.SAVE_ITEM, item, options)
             .map((response: Response) => response.json() as boolean);
     }
 
